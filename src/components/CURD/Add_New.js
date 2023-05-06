3b90b0decff4ac3aae67d714bfd42faf083ba634
Ashish Chaudhari <ashishchaudharistudy1857@gmail.com>
2023-05-06 13:50:37 +0530

    newchange

---
+++ src/components/CURD/Add_New.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { Expenseform } from "../Expenseform_user/Expenseform";
+const Add_New = (props) => {
+  // const[prevUsers ,setUsers]=useState(props.Users)
+  const {setUsers} =props;
+   
+  const addExpenseHandler = (event) => {
+
+    event.preventDefault();
+    const newExpense = {
+      date: event.target.date.value,
+      purpose: event.target.purpose.value,
+      amount: event.target.price.value,
+    };
+    console.log("hey");
+    console.log(newExpense);
+    setUsers((prevUsers) => [...prevUsers, newExpense]);
+    console.log(props.Users);
+    event.target.reset();
+    console.log(props.Users);
+  };
+  return (
+    <>
+      <Expenseform add={addExpenseHandler}></Expenseform>
+    </>
+  );
+}
+export {Add_New};
